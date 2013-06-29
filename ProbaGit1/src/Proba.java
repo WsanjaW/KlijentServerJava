c1846aae58c980c70dc008dd5ff991a79fdc407d
Sanja Katanic <sanjak_5@hotmail.com>
2013-06-29 17:52:24 +0200

    Server proba

--- ProbaGit1/src/Proba.java
+++ ProbaGit1/src/Proba.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class Proba {
 
@@ -6,6 +13,20 @@ public class Proba {
 	 */
 	public static void main(String[] args) {
 		System.out.println("DD");
+		try {
+			ServerSocket ss=new ServerSocket(8090);
+			while (true) {
+				Socket szk = ss.accept();
+				BufferedReader ut = new BufferedReader(new InputStreamReader(szk.getInputStream()));
+				DataOutputStream izt = new DataOutputStream(szk.getOutputStream());
+				System.out.println("Klijent: "+ ut.readLine());
+				izt.writeBytes("HHJKKL");
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
